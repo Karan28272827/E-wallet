dec83cea8cfd68d6d0468cec754ce3aba8f07fb0
Karan Paigude <paigudekaran2827@gmail.com>
2025-11-12 17:12:52 +0530

    power commit

--- backend/E-wallet-backend/src/controllers/auth.controller.js
+++ backend/E-wallet-backend/src/controllers/auth.controller.js
@@ -36,13 +36,13 @@ export const loginUser = async (req, res, next) => {
     // find user
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid email" });
     }
 
     // check password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid password" });
     }
 
     // create JWT
